6b9fa7cd704c5147452a195507247fa60499352e
jyothinavya23 <159226897+jyothinavya23@users.noreply.github.com>
2024-02-26 05:16:31 -0500

    Update simple.java

--- simple.java
+++ simple.java
@@ -4,6 +4,6 @@ public class simple java
 	{
 		System.out.println("hello java");
 		System.out.println("welcome to spt);
-		System.out.println("java world");
+		System.out.println("welcome to java world");
 	}
 }
